8476c2bf604d7a14d04cf23d39ce92c8e656d049
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2023-01-30 12:31:42 +0100

    Fetch product list, cart item counter & display

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /* - - - - Open / Close hamburger menu - - - - */
+
 const hamburgerMenu = document.getElementById("hamburger-menu")
 const nav = document.getElementById("nav")
 
@@ -14,101 +15,147 @@ const hamburgerMenuToggle = (e) => {
 window.addEventListener("click", hamburgerMenuToggle)
 
 
-/* - - - - Insert products - - - - */
-const products = [
-    {
-        id: 14,
-        name: "Málna",
-        description: "Kézzel szedett egészség",
-        picture: "img/pexels-pixabay-326179.jpg",
-        price: 3800,
-        inStock: true,
-    },
-    {
-        id: 27,
-        name: "Áfonya",
-        description: "Kézzel szedett egészség",
-        picture: "img/pexels-veeterzy-70862.jpg",
-        price: 3250,
-        inStock: true,
-    },
-    {
-        id: 31,
-        name: "Szeder",
-        description: "Kézzel szedett egészség",
-        picture: "img/pexels-pixabay-257834.jpg",
-        price: 1700,
-        inStock: true,
-        variations: ["fehér", "fekete"]
-    },
-]
+
+/* - - - - Products load- - - - */
+
+let products = []
 
 const productsSection = document.getElementById("products")
 
-products.forEach(product => {
+fetch("https://hur.webmania.cc/products.json")
+    .then(response => response.json())
+    .then(data => {
+        products = data.products
+        console.log(data.products)
 
-    productsSection.innerHTML += `
-        <div>
-            <h2>${product.name}</h2>
-            <p>${product.description}</p>
-            <div class=product-image-container>
-                <img src="${product.picture}" alt="">
-            </div>
-            <h3>${product.price} Ft</h3>
-            <a id=${product.id} 
-            class="add-to-cart">Kosárba</a>
-        </div>
-    `
-})
+        products.forEach(product => {
+            productsSection.innerHTML += `
+                <div>
+                    <h2>${product.name}</h2>
+                    <p>${product.description}</p>
+                    <div class=product-image-container>
+                        <img src="${product.picture}" alt="">
+                    </div>
+                    <h3>${product.price.toLocaleString()} Ft</h3>
+                    </div>
+                    `
+            if (product.stock) {
+                productsSection.innerHTML += `
+                <a id=${product.id} 
+                        class="add-to-cart">Kosárba</a>`
+            } else {
+                productsSection.innerHTML += `Nem rendelhető`
+
+            }
+        })
+
+
+
+        /* - - - - Cart item counter - - - - */
+
+        const addToCartButtons = document.querySelectorAll(".add-to-cart")
+
+        addToCartButtons.forEach(addToCartButton => {
+            addToCartButton.addEventListener("click", addToCart)
+        })
+
+    })
+    .catch(error => console.error(error))
+
+
 
 /* - - - - Cart handle - - - - */
 
 const cart = {}
 
-const addToCartButtons = document.querySelectorAll(".add-to-cart")
+const addToCart = event => {
+    let target = event.target.dataset.id
 
-/* for (let i = 0; i < addToCartButtons.length; i++) {
-    addToCartButtons[i].addEventListener("click", function (event) {
-        if (cart[event.target.id] === undefined) {
-            cart[event.target.id] = 1
-        } else {
-            cart[event.target.id]++
-        }
-    })
-} */
-/* - - - - Cart item counter - - - - */
-addToCartButtons.forEach(addToCartButton => {
-    addToCartButton.addEventListener("click", event => {
-        if (cart[event.target.id] === undefined) {
-            cart[event.target.id] = 1
-        } else {
-            cart[event.target.id]++
-        }
-    })
-})
+    if (event.target.id) {
+        target = event.target.id
+    }
 
-/* - - - - Cart items display - - - - */
-const cartIcon = document.getElementById("cart-icon")
-const cartContainer = document.querySelector(".cart-container")
 
-cartIcon.addEventListener("click", () => {
+    if (cart[target] === undefined) {
+        cart[target] = 1
+    } else {
+        cart[target]++
+    }
+}
+
+
+
+const refreshCartItems = () => {
+
+    // a cart-items kiürítése, hogy a kattintásokkal a duplikását elkerüljük
+    cartItems.innerHTML = ""
+    let total = 0
+
+
 
     for (const item in cart) {
-        console.log(item, cart[item])
-        console.log(products.find(product => product.id === item * 1).name, cart[item])
+        const currentProduct = products.find(product => product.id === item * 1)
+        cartItems.innerHTML += `
+        <li>
+            <div>
+                <button class = "plus-button" data-id=${currentProduct.id}>+</button>
+                <p>${cart[item]} db</p>
+                <p>${currentProduct.name}</p>
+                <p>X</p>
+                <p>${currentProduct.price} Ft/db</p>
+            </div>
+            <div><strong>${cart[item] * currentProduct.price} Ft</strong></div>
+        </li>
+        `
+        total += currentProduct.price * cart[item]
     }
+
+
+
+    cartItems.innerHTML += `
+    <li>
+        <p>Összesen:</p> 
+        <p>${total.toLocaleString()} Ft</p>
+    </li>`
+
+}
+
+
+
+/* - - - - Cart items display - - - - */
+
+const cartIcon = document.getElementById("cart-icon")
+const cartContainer = document.querySelector(".cart-container")
+const cartItems = document.querySelector(".cart-items")
+
+cartItems.addEventListener("click", (event) => {
+    addToCart(event)
+    refreshCartItems()
 })
 
+
+
 /* - - - - Cart button toggle - - - - */
+
 const cartContainerToggler = (e) => {
-    console.log(e.target)
-    if (e.target.classList == "fi-shopping-cart") {
+    if (e.target.classList == "fi-shopping-cart" || e.target.classList == "plus-button") {
         cartContainer.classList.add("cart-container-active")
         cartIcon.classList.add("fi-x")
-
+        refreshCartItems()
     } else {
         cartContainer.classList.remove("cart-container-active")
         cartIcon.classList.remove("fi-x")
     }
 }
 window.addEventListener("click", cartContainerToggler)
+
+
+
+
+
+
+/* 
+window.addEventListener("click", (e) => {
+    console.log(e.target)
+})
+ */
